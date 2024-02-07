e06ca54b1cdeda7b5ff02a57122def2305d42a63
RMF-AndyPlayz <andyjia2500@gmail.com>
2024-02-06 20:20:41 -0800

    progress so far

---
+++ Assets/Andy (@rmfz)'s Folder/Social Credit Kahoot/Scripts/rmfz_RunSocialCreditCutscene.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//This script is written by Andy (@rmfz)
+//Please contact me if you make any changes, or need to discuss something in this script!
+public class rmfz_RunSocialCreditCutscene : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    [SerializeField] private int countdownSecs = 5;
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private Image loadingCircle;
+
+    private void Start()
+    {
+        StartCoroutine(StartCutsceneSequence(nextSceneName));
+    }
+
+    /// <summary>
+    /// Begins the countdown, the circle loading thingy, and whatnot.
+    /// </summary>
+    IEnumerator StartCutsceneSequence(string nextSceneName)
+    {
+        float loadingCircleStartValue = countdownSecs;
+
+        while(countdownSecs > 0)
+        {
+            timerText.text = $"{countdownSecs}";
+
+            float elapsedTime = Time.deltaTime;
+            float startFill = 1 - (countdownSecs / loadingCircleStartValue);
+            float endFill = 1 - ((countdownSecs - 1) / loadingCircleStartValue);
+
+            while(elapsedTime < 1)
+            {
+                elapsedTime += Time.deltaTime;
+                loadingCircle.fillAmount = Mathf.Lerp(startFill, endFill, elapsedTime);
+                yield return null;
+            }
+
+            loadingCircle.fillAmount = 1 - ((float) countdownSecs / loadingCircleStartValue); ;
+            countdownSecs--;
+        }
+
+        loadingCircle.fillAmount = 1;
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
---
+++ Assets/Andy (@rmfz)'s Folder/_General Utility Stuff/Scripts/rmfz_AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+//This script is written by Andy (@rmfz)
+//Please contact me if you make any changes, or need to discuss something in this script!
+public class rmfz_AudioManager : MonoBehaviour
+{
+    
+    public static void PlaySound()
+    {
+
+    }
+
+    public static void PlayMusic()
+    {
+
+    }
+
+    public static void PlayMusicOnLoop()
+    {
+
+    }
+
+}
