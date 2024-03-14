d0dfc039fc497d1a708960b4d92dce53b6488c1b
mivan <catnipiscatweed@gmail.com>
2024-03-13 19:11:31 -0700

    added timing fun

--- Assets/HaveYouClickedToday.cs
+++ Assets/HaveYouClickedToday.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class HaveYouClickedToday : MonoBehaviour
 {
     [SerializeField] Animator anim;
-    [SerializeField] float m_Length;
     
     const string PRESS_ANIM = "clicked_screen";
 
     void OnMouseDown()
     {
         anim.SetBool(PRESS_ANIM, true);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (hit.collider != null) {
+            Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider.gameObject.name == "nuke7")
+            {
+                IsClicked.mary_headshot = false;
+                
+                anim.SetBool(PRESS_ANIM, true);
+                
+                GameStateManager.Lose();
+            }
+        }
     }
 }
